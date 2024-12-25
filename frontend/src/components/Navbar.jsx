d7b96bbee7d3338d8103d2f35ddceb8d30bb09a5
Sumama <sumamabutt500@gmial.com>
2024-12-25 18:13:57 +0500

    mod

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,8 +5,8 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
-    <nav className='navbar fixed top-0 w-full h-20  flex justify-between bg-opacity-0 backdrop-blur-md'>
-        <House className='icon h-10 w-10 mx-3 mt-5 text-black font-bold'/>
+    <nav className='navbar fixed top-0 w-full h-20  flex justify-between bg-opacity-0 backdrop-blur-md shadow-md'>
+        <House className='icon h-10 w-10 mx-3 mt-5 text-white font-bold'/>
         <ul className='flex justify-center items-center px-60 space-x-6'>
            <Link to="/">        <li  >Home    </li></Link>
            <Link to="/about">   <li >About    </li></Link>
